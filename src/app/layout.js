677ab197602d30854fae6b14721922f14abe921d
Juan Cruz <astronaut@cruzantonio.net>
2026-07-03 17:46:41 -0700

    fixed string

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,6 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
   const conn = await dbConnect();
-  console.log(conn);
   return (
     <html lang="en">
       <body>
--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function dbConnect() {
     try {
-        const conn = await mongoose.connect(String(process.env.MONGO_DB_CONNECTION_STRING));
+        const conn = await mongoose.connect(String(process.env.MONGODB_URI));
         return conn;
     } catch (e) {
         throw new Error(e);
